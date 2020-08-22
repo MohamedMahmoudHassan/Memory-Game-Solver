c9e99a5f843f68b10e2f0e1abf600e5d6b1776f4
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-08-22 18:18:40 +0200

    Create Card class

--- cards.py
+++ cards.py
@@ -1,11 +1,27 @@
 from pynput.mouse import Button, Controller
 import time
 
-
 mouse = Controller()
 
-def flip_card(coords):
-    time.sleep(2)
-    mouse.position = coords
-    mouse.press(Button.left)
-    mouse.release(Button.left)
+
+class Card:
+    def __init__(self, pos, first_card_coords, delta, is_first_of_pair):
+        self.pos = pos
+        self.coords = (first_card_coords[0] + delta[0] * pos[0],
+                       first_card_coords[1] + delta[1] * pos[1])
+        self.sleep_time = 1.5 if is_first_of_pair else 0.1
+
+    def flip(self):
+        time.sleep(self.sleep_time)
+        mouse.position = self.coords
+        mouse.press(Button.left)
+        mouse.release(Button.left)
+
+
+def generate_cards(cards, first_card_coords, game_dim, delta):
+    is_first_of_pair = True
+    for x in range(game_dim[0]):
+        for y in range(game_dim[1]):
+            card = Card((x, y), first_card_coords, delta, is_first_of_pair)
+            cards.append(card)
+            is_first_of_pair = not is_first_of_pair
--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from monitor import *
 from cards import *
 
+game_dim = (9, 6)
+
 wait_monitoring_start()
 coords = []
 get_coord(coords)
 get_coord(coords)
 
-first_pos = (min(coords[0][0], coords[1][0]), min(coords[0][1], coords[1][1]))
-flip_card(first_pos)
+delta = ((coords[1][0] - coords[0][0]) / (game_dim[0] - 1),
+         (coords[1][1] - coords[0][1]) / (game_dim[1] - 1))
+
+
+cards = []
+generate_cards(cards, coords[0], game_dim, delta)
+
+for x in cards:
+    x.flip()
+
