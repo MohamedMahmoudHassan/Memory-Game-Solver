0ab1b9995c9cfa5ab350c165a459805ac4b073a3
Mohamed <mohamedmahmoudalihassan@gmail.com>
2020-08-22 10:18:10 +0200

    Get coords of mouse

---
+++ main.py
@@ -0,0 +1,8 @@
+from monitor import *
+
+wait_monitoring_start()
+coords = []
+get_coord(coords)
+get_coord(coords)
+
+print(coords)
---
+++ monitor.py
@@ -0,0 +1,24 @@
+from pynput import mouse, keyboard
+
+
+def get_coord(coords):
+    def on_click(x, y, button, pressed):
+        if pressed:
+            coords.append((x, y))
+            return False
+
+    with mouse.Listener(on_click=on_click) as listener:
+        listener.join()
+
+
+def wait_monitoring_start():
+    def on_press(key):
+        try:
+            if(key.char == 's'):
+                return False
+        except AttributeError:
+            print('special key {0} pressed'.format(key))
+
+    # Collect events until released
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
